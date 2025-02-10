a62c73e4706b9d8e9734dba4c6ce06cf584ec606
Chintan Patel <chintanpatelit024@gmail.com>
2025-02-10 11:53:42 +0530

    Added: Player mouse click detection

--- Source/Event/EventManager.cpp
+++ Source/Event/EventManager.cpp
@@ -1,4 +1,5 @@
 #include "../../Header/Event/EventManager.h"
+#include "iostream"
 
 namespace EventM {
     void EventManager::pollEvents(RenderWindow* game_window) {
@@ -11,11 +12,25 @@ namespace EventM {
             if (isKeyPressed(sf::Keyboard::Escape)) {
                 game_window->close();
             }
+            // Handle left mouse button click
+            if (isLeftMouseButtonClicked())
+            {
+                sf::Vector2i position = sf::Mouse::getPosition(*game_window);
+
+                // Log the mouse position
+                std::cout << "Left mouse click at: " << position.x << ", " << position.y << std::endl;
+            }
         }
     }
 
     bool EventManager::isKeyPressed(sf::Keyboard::Key key) {
         // Detect if a specific key is pressed
         return sf::Keyboard::isKeyPressed(key);
+    }
+
+    bool EventManager::isLeftMouseButtonClicked()
+    {
+        // Detect if the left mouse button is clicked
+        return (sf::Mouse::isButtonPressed(sf::Mouse::Left));
     }
 }
